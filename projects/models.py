ea55f44efe8d7c6343e72b15ae36c32b62ffbc1c
jesus Álvarez <113483985+warzone12@users.noreply.github.com>
2024-04-06 22:44:23 -0300

    Update models.py

--- projects/models.py
+++ projects/models.py
@@ -5,7 +5,7 @@ class Project(models.Model):
     nombre_usuario = models.CharField(max_length=200)
     producto = models.CharField(max_length=100, default='')
     categoria = models.TextField()
-    marca = models.CharField(max_length=200)
+    marca = models.CharField(max_length=205)
     precio = models.CharField(max_length=170, default='0.00')
     fecha = models.DateTimeField(auto_now_add=True)
     
